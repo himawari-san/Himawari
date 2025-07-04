3a44c2eb6d142ac64b31c508f62739a20cd779a1
himawari-san <himawari@ninjal.ac.jp>
2025-07-04 10:10:41 +0900

    Always use bundled JRE

--- src/main/dist/himawari.bat
+++ src/main/dist/himawari.bat
@@ -1,10 +1,1 @@
-java -jar himawari.jar
-
-:: GUI の言語を日本語にする
-:: java -Duser.language=ja -jar himawari.jar
-
-:: GUI の言語を英語にする
-:: java -Duser.language=en -jar himawari.jar
-
-:: OSにインストールされている Java を使用
-:: java -jar himawari.jar
+jre\64\bin\java -jar himawari.jar
